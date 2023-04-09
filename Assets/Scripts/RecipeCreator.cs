b34bc35fed98a39e90dbadbb1182a4a2631dff5c
AnyaLeo <ania.leonova@gmail.com>
2023-04-08 22:41:31 -0700

    Lesson 4. Started making unique dish names

--- Assets/Scripts/RecipeCreator.cs
+++ Assets/Scripts/RecipeCreator.cs
@@ -6,10 +6,20 @@ public class RecipeCreator : MonoBehaviour
 {
     public List<GameObject> ingredients;
 
+    public string[] adjectives;
+    public string[] nouns; 
+
     // Start is called before the first frame update
     void Start()
     {
         ingredients = new List<GameObject>();
+
+        // This is how we load text files from the Resources folder in Unity
+        TextAsset textAdjectives = Resources.Load<TextAsset>("adjectives");
+        adjectives = textAdjectives.text.Split(',');
+
+        TextAsset textNouns = Resources.Load<TextAsset>("nouns");
+        nouns = textNouns.text.Split(',');
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -20,5 +30,12 @@ public class RecipeCreator : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         ingredients.Remove(collision.gameObject);
+    }
+
+    public void CreateFood()
+    {
+        print("Created food");
+
+        // (Adjective) + (one of the ingredients on the board) + (noun for a food object)
     }
 }
--- Assets/Showcase/Scripts/T_RecipeCreator.cs
+++ Assets/Showcase/Scripts/T_RecipeCreator.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+
+public struct RecipeIngredients
+{
+    string ingredient1;
+    string ingredient2;
+    string ingredient3;
+
+    public RecipeIngredients(string ing1, string ing2, string ing3)
+    {
+        ingredient1 = ing1;
+        ingredient2 = ing2;
+        ingredient3 = ing3;
+    }
+
+    public override string ToString() =>
+        $"1: {ingredient1}; 2: {ingredient2}; 3: {ingredient3};";
+}
 
 public class T_RecipeCreator : MonoBehaviour
 {
-    public int minItemsForDish = 2;
-    public int maxItemsForDish = 6;
+    public int itemsRequiredForDish = 3;
 
     public Text dishName;
 
     public List<GameObject> ingredients;
+
+    public Dictionary<RecipeIngredients, string> recipeBook;
 
     public string[] listOfAdjectives;
     private string[] listOfNouns;
     private void Start()
     {
         ingredients = new List<GameObject>();
+        recipeBook = new Dictionary<RecipeIngredients, string>();
 
         TextAsset adjectives = Resources.Load<TextAsset>("adjectives");
         listOfAdjectives = adjectives.text.Split(',');
@@ -46,22 +66,52 @@ public class T_RecipeCreator : MonoBehaviour
 
     public void CreateFood()
     {
-        bool notEnoughIngredients = ingredients.Count <= minItemsForDish && ingredients.Count >= maxItemsForDish;
+        bool notEnoughIngredients = ingredients.Count != itemsRequiredForDish;
         if (notEnoughIngredients)
         {
             Debug.LogWarning("Trying to create a dish with " + gameObject.transform.childCount + " number of ingredients, which is either not enough or too much.");
             return;
         }
 
-        // create a funny name for the dish
-        int randomAdjective = Random.Range(0, listOfAdjectives.Length);
-        int randomIngredient = Random.Range(0, ingredients.Count);
-        int randomNoun = Random.Range(0, listOfNouns.Length);
+       
 
-        string foodName = listOfAdjectives[randomAdjective] + " " + ingredients[randomIngredient].name + " " + listOfNouns[randomNoun];
+        // add to the recipe book
+        string[] ingredientsArray = new string[itemsRequiredForDish];
+        for (int i = 0; i < itemsRequiredForDish; i++)
+        {
+            ingredientsArray[i] = ingredients[i].name;
+        }
+
+        // make sure you include "using System" up top
+        Array.Sort(ingredientsArray);
+
+        RecipeIngredients recipeIngredients = new RecipeIngredients(ingredientsArray[0], ingredientsArray[1], ingredientsArray[2]);
+        print(recipeIngredients.ToString());
+
+
+        bool recipeAlreadyExists = recipeBook.ContainsKey(recipeIngredients);
+        string foodName;
+        if (recipeAlreadyExists)
+        {
+            foodName = recipeBook[recipeIngredients];
+        }
+        else
+        {
+            // create a funny name for the dish
+            int randomAdjective = UnityEngine.Random.Range(0, listOfAdjectives.Length);
+            int randomIngredient = UnityEngine.Random.Range(0, ingredients.Count);
+            int randomNoun = UnityEngine.Random.Range(0, listOfNouns.Length);
+
+            foodName = listOfAdjectives[randomAdjective] + " " + ingredients[randomIngredient].name + " " + listOfNouns[randomNoun];
+
+            // record it in the recipe book
+            recipeBook.Add(recipeIngredients, foodName);
+        }
+
+        // Show the funny name to the user
 
         dishName.text = "Congratulations, you created a\n" + foodName;
 
-        // delete the ingredients and show the funny name to the user
+        // TODO: delete the ingredients 
     }
 }
